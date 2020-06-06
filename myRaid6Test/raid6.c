1879f0433a5a471102e17b08a11b27a9e5e7938f
sl <871958943@qq.com>
2020-06-06 04:52:47 -0400

    暂存

--- myRaid6Test/raid6.c
+++ myRaid6Test/raid6.c
@@ -27,10 +27,10 @@
   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 **********************************************************************/
 
-#include<stdio.h>
-#include<stdint.h>
-#include<string.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <isa-l/crc.h>
 #include <isa-l/crc64.h>
@@ -43,130 +43,88 @@
 #include <isa-l/test.h>
 
 #define TEST_SOURCES 10
-#  define GT_L3_CACHE  32*1024*1024	/* some number > last level cache */
-#define TEST_LEN     ((GT_L3_CACHE / TEST_SOURCES) & ~(64-1))
+#define GT_L3_CACHE 32 * 1024 * 1024 /* some number > last level cache */
+#define TEST_LEN ((GT_L3_CACHE / TEST_SOURCES) & ~(64 - 1))
 // #define TEST_LEN 1024
-#define TEST_MEM ((TEST_SOURCES + 2)*(TEST_LEN))
+#define TEST_MEM ((TEST_SOURCES + 2) * (TEST_LEN))
 #ifndef TEST_SEED
-# define TEST_SEED 0x1234
+#define TEST_SEED 0x1234
 #endif
-#  define TEST_TYPE_STR "_cold"
-
-// # define CLOCK_ID CLOCK_MONOTONIC
-
-// static inline long long get_time(void) {
-// 	struct timespec time;
-// 	long long nano_total;
-// 	 clock_gettime(CLOCK_ID, &time);
-// 	 nano_total = time.tv_sec;
-// 	 nano_total *= NANO_SCALE;
-// 	 nano_total += time.tv_nsec;
-// 	 return nano_total;
-// }
-
-int ref_multi_pq(int vects, int len, void **array)
-{
-	int i, j;
-	unsigned char p, q, s;
-	unsigned char **src = (unsigned char **)array;
-  
-  for (i = 0; i < len; i++) {
-		q = p = src[vects - 3][i];
-
-		for (j = vects - 4; j >= 0; j--) {
-			p ^= s = src[j][i];
-			q = s ^ ((q << 1) ^ ((q & 0x80) ? 0x1d : 0));	// mult by GF{2}
-		}
-
-		src[vects - 2][i] = p;	// second to last pointer is p
-		src[vects - 1][i] = q;	// last pointer is q
-	}
-	return 0;
-}
+#define TEST_TYPE_STR "_cold"
+#define TEST_TIMES 20
 
 // Generates pseudo-random data
 
-void rand_buffer(unsigned char *buf, long buffer_size)
+static void rand_buffer(unsigned char *buf, long buffer_size)
 {
-	long i;
-	for (i = 0; i < buffer_size; i++)
-		buf[i] = rand();
+  long i;
+  for (i = 0; i < buffer_size; i++)
+    buf[i] = rand();
+}
+
+static inline int do_test_pq_check(void **buffs)
+{
+  for (int i = 0; i < TEST_TIMES; i++)
+  {
+    // 校验
+    // ret = pq_check_base(TEST_SOURCES + 2, TEST_LEN, buffs);
+    int ret = pq_check(TEST_SOURCES + 2, TEST_LEN, buffs);
+    // ret = pq_check_sse(TEST_SOURCES + 2, TEST_LEN, buffs);
+  }
 }
 
 int main(int argc, char *argv[])
 {
-    // pq_check()
-    long long sum = 0;
-    struct perf start;
-    int i, j, k, ret, fail = 0;
-    void *buffs[TEST_SOURCES + 2];
-    char c;
-    char *tmp_buf[TEST_SOURCES + 2];
-    int serr, lerr;
+  // pq_check()
+  long long sum = 0;
+  struct perf start;
+  int i, j, k, ret, fail = 0;
+  void *buffs[TEST_SOURCES + 2];
+  char c;
+  char *tmp_buf[TEST_SOURCES + 2];
+  int serr, lerr;
 
-    printf("Test pq_check_test %d sources X %d bytes\n", TEST_SOURCES, TEST_LEN);
+  printf("Test pq_check_test %d sources X %d bytes\n", TEST_SOURCES, TEST_LEN);
 
-    srand(TEST_SEED);
+  srand(TEST_SEED);
 
-    // Allocate the arrays
-    for (i = 0; i < TEST_SOURCES + 2; i++)
+  // Allocate the arrays
+  for (i = 0; i < TEST_SOURCES + 2; i++)
+  {
+    void *buf;
+    // posix_memalign返回TEST_LEN长度的buffer，地址是16的倍数
+    if (posix_memalign(&buf, 16, TEST_LEN))
     {
-      void *buf;
-      // posix_memalign返回TEST_LEN长度的buffer，地址是16的倍数
-      if (posix_memalign(&buf, 16, TEST_LEN))
-      {
-        printf("alloc error: Fail");
-        return 1;
-      }
-      buffs[i] = buf;
+      printf("alloc error: Fail");
+      return 1;
     }
+    buffs[i] = buf;
+  }
+  // Test rand1
+  // 产生随机数
+  for (i = 0; i < TEST_SOURCES + 2; i++)
+    rand_buffer((unsigned char *)buffs[i], TEST_LEN);
+  // 编码
+  pq_gen_avx(TEST_SOURCES + 2, TEST_LEN, buffs);
+  long long starttime = get_time();
+  // pid_t pid[]
 
-    // Test rand1
-    // 产生随机数
-    for (i = 0; i < TEST_SOURCES + 2; i++)
-      rand_buffer((unsigned char *)buffs[i], TEST_LEN);
+  do_test_pq_check(buffs);
+  // for (int i = 0; i < TEST_TIMES; i++)
+  // {
+  //   // 校验
+  //   // ret = pq_check_base(TEST_SOURCES + 2, TEST_LEN, buffs);
+  //   pq_check(TEST_SOURCES + 2, TEST_LEN, buffs);
+  //   // ret = pq_check_sse(TEST_SOURCES + 2, TEST_LEN, buffs);
+  // }
+  long long endtime = get_time();
+  long long nsec = endtime - starttime;
+  long long usec = nsec / 1000000;
 
-    // ret = pq_check(TEST_SOURCES + 2, TEST_LEN, buffs);
-    // 编码
-    ref_multi_pq(TEST_SOURCES + 2, TEST_LEN, buffs);
-    int test_times = 1;
-    // 使用自带的测试程序
-    BENCHMARK(&start, test_times, pq_check(TEST_SOURCES + 2, TEST_LEN, buffs););
-    perf_print(start, (long long)TEST_MEM);
-    time_t starttime2 = clock();
-    long long starttime = get_time();
-    for (int i = 0; i < test_times; i++)
-    {
-        // ref_multi_pq(TEST_SOURCES + 2, TEST_LEN, buffs);
-        // pq_gen_avx(TEST_SOURCES + 2, TEST_LEN, buffs);
-        // 校验
-        // ret = pq_check_base(TEST_SOURCES + 2, TEST_LEN, buffs);
-        ret = pq_check(TEST_SOURCES + 2, TEST_LEN, buffs);     
-        // ret = pq_check_sse(TEST_SOURCES + 2, TEST_LEN, buffs);   
-        // printf("有效数据量%dx%d\n", TEST_SOURCES, TEST_LEN);
-    }
-    long long endtime = get_time();
-    time_t endtime2 = clock();
-    long long clock_ms = endtime2 - starttime2;
-    long long nsec = endtime - starttime;
-    long long usec = nsec / 1000000;
-    
-    printf("clock():%lld\n", clock_ms);
-    printf("clock_gettime():%lld\n", nsec);
-    printf("校验耗时%lldus\n", usec);
-    printf("吞吐率%fMB/s\n", ((GT_L3_CACHE / 1024 / 1024) * test_times) * 1.0 / nsec * 1000000000);
-    printf("吞吐率%fMB/s\n", ((GT_L3_CACHE / 1024 / 1024) * test_times) * 1.0 / clock_ms * 1000000);
-    // printf("吞吐率%fMbps\n", (TEST_LEN / 1024 / 1024 * TEST_SOURCES *  8 * 1.0 * test_times) / sum * 1000);
-    // c = ((char *)(buffs[0]))[TEST_LEN - 2];
-	// ((char *)(buffs[0]))[TEST_LEN - 2] = c ^ 0x1;
-	// ret = pq_check(TEST_SOURCES + 2, TEST_LEN, buffs);
-	// if (ret == 0) {
-	// 	fail++;
-	// 	printf("\nFail corrupt buffer test, passed when should have failed\n");
-	// }
-	// ((char *)(buffs[0]))[TEST_LEN - 2] = c;	// un-corrupt buffer
+  printf("clock_gettime():%lld\n", nsec);
+  printf("校验耗时%lldus\n", usec);
+  printf("吞吐率%fMB/s\n", ((GT_L3_CACHE / 1024 / 1024) * TEST_TIMES) * 1.0 / nsec * 1000000000);
+  // printf("吞吐率%fMbps\n", (TEST_LEN / 1024 / 1024 * TEST_SOURCES *  8 * 1.0 * test_times) / sum * 1000);
 
-
-	return 0;
-
+  return 0;
 }
