67967b82332dac56def820ec1d35bfa9eeb39f9a
sl <871958943@qq.com>
2020-08-06 23:20:16 -0400

    代码重构

--- myRaid6Test/raid6.c
+++ myRaid6Test/raid6.c
@@ -48,18 +48,42 @@
 #include <isa-l/test.h>
 #include <errno.h>
 
-#define TEST_SOURCES 8  // 磁盘数量
+/**
+ * 修改说明
+ * 可以修改
+ * TEST_SOURCES     raid6中的磁盘数量
+ * TEST_LEN         校验时使用块大小
+ * TEST_SEED        随机数种子
+ * TEST_TIMES       定义了ALL_IN_CACHE同一组数据在子线程中被重复校验的次数
+ * PID_NUM          子线程数量，等与0时测试单核性能
+ * ALL_IN_CACHE     所有子进程访问同一地址空间，总地址空间较小的情况下所有数据将来自Cache，此时只测试CPU计算性能，请使用者自己确保Cache大小足够
+ * CHECK_BASELINE   校验函数是否使用baseline函数
+ * 
+ * 谨慎修改
+ * UNIT             每次申请子进程，逐步减少的循环次数，需保证UNIT * (PID_NUM - 1) <= TEST_TIMES
+ * 
+ * if you know what you are doing
+ * GT_L3_CACHE      代表L3级Cache大小，需要与下一行定义的TEST_LEN配合使用，保证校验函数访问的数据均来自Cache，需定义ALL_IN_CACHE
+ * 
+ * 请勿修改
+ * TEST_MEM         定义了ALL_IN_CACHE后待校验数据占用的内存空间大小，
+  */
+
+#define TEST_SOURCES 8  // 磁盘数量，实际使用的磁盘数量还需加2，用于存储PQ校验数据
 // #define GT_L3_CACHE 32 * 1024 * 1024 /* some number > last level cache 总磁盘容量*/
-// #define TEST_LEN ((GT_L3_CACHE / TEST_SOURCES) & ~(64 - 1)) // 单个磁盘容量
-#define TEST_LEN 2 * 1024 * 1024
-#define TEST_MEM ((TEST_SOURCES + 2) * (TEST_LEN))
+// #define TEST_LEN ((GT_L3_CACHE / (TEST_SOURCES + 2)) & ~(64 - 1)) // 单个磁盘容量
+#define TEST_LEN 1 * 1024 * 1024 
+#define TEST_MEM ((TEST_SOURCES + 2) * (TEST_LEN))  // 定义了ALL_IN_CACHE后待校验数据占用的内存空间大小
 #ifndef TEST_SEED
 #define TEST_SEED 0x1234
 #endif
 #define TEST_TYPE_STR "_cold"
-#define TEST_TIMES 1024
+#define TEST_TIMES 10240
 #define PID_NUM 32  // 线程数
-#define UNIT 0     // 每次申请子进程，逐步减少的循环次数
+#define UNIT 2     // 每次申请子进程，逐步减少的循环次数
+
+#define ALL_IN_CACHE  // 各个线程访问相同地址空间，总地址空间较小的情况下所有数据将来自Cache，只测试CPU计算性能
+// #define CHECK_BASELINE  // 校验时调用pq_check_base，注释掉则调用pq_check_sse
 
 // Generates pseudo-random data
 static void rand_buffer(unsigned char *buf, long buffer_size)
@@ -74,9 +98,13 @@ static inline int do_test_pq_check(void **buffs, int test_times)
   for (int i = 0; i < test_times; i++)
   {
     // 校验
-    // ret = pq_check_base(TEST_SOURCES + 2, TEST_LEN, buffs);
-    pq_check(TEST_SOURCES + 2, TEST_LEN, buffs);
+#ifdef CHECK_BASELINE
+    pq_check_base(TEST_SOURCES + 2, TEST_LEN, buffs);
+#else
+    // printf("debug\n");
     // pq_check_sse(TEST_SOURCES + 2, TEST_LEN, buffs);
+    pq_check_base(TEST_SOURCES + 2, TEST_LEN, buffs);
+#endif
   }
 }
 
@@ -100,7 +128,13 @@ int main(int argc, char *argv[])
   {
     void *buf;
     // posix_memalign返回TEST_LEN长度的buffer，地址是16的倍数
-    if (posix_memalign(&buf, 16, TEST_LEN))
+    if (
+#ifdef ALL_IN_CACHE
+        posix_memalign(&buf, 16, TEST_LEN)
+#else
+        posix_memalign(&buf, 16, TEST_LEN * TEST_TIMES)
+#endif
+    )
     {
       printf("alloc error: Fail");
       return 1;
@@ -109,10 +143,25 @@ int main(int argc, char *argv[])
   }
   // Test rand
   // 产生随机数
+  printf("rand\n");
   for (i = 0; i < TEST_SOURCES + 2; i++)
+  {
+#ifdef ALL_IN_CACHE
     rand_buffer((unsigned char *)buffs[i], TEST_LEN);
+#else
+    // too slow
+    // rand_buffer((unsigned char *)buffs[i], TEST_LEN * TEST_TIMES);
+#endif
+  }
+  printf("rand OK\n");
   // raid6编码
+  printf("encode\n");
+#ifdef ALL_IN_CACHE
   pq_gen_avx(TEST_SOURCES + 2, TEST_LEN, buffs);
+#else
+  pq_gen_avx(TEST_SOURCES + 2, TEST_LEN * TEST_TIMES, buffs);
+#endif
+  printf("encode OK\n");
   // BENCHMARK(&start, TEST_TIMES, pq_check(TEST_SOURCES + 2, TEST_LEN, buffs));
   // perf_print(start, (long long)TEST_MEM);
   pid_t pid[PID_NUM];
@@ -125,47 +174,76 @@ int main(int argc, char *argv[])
     real_times += TEST_TIMES - i * UNIT;
   }
   // get_time()由isa-l库定义，调用了clock_gettime，结果以纳秒为单位
-  long long starttime = get_time();
-  for (int i = 0; i < PID_NUM; i++)
+  long long starttime;
+  long long endtime;
+  if (PID_NUM == 0) // single core test
   {
-    pid[i] = fork();
-    if (pid[i] > 0)
-    {
-      continue;
-    }
-    else if (pid[i] == 0)
-    {
-      do_test_pq_check(buffs, TEST_TIMES - i * UNIT);
-      exit(0);
-    }
-    else
-    {
-      printf("fork() failed\n");
-      break;
-    }
+    starttime = get_time();
+#ifdef ALL_IN_CACHE
+    do_test_pq_check(buffs, TEST_TIMES);
+#else
+#ifdef CHECK_BASELINE
+    pq_check_base(TEST_SOURCES + 2, TEST_LEN * TEST_TIMES, buffs);
+#else
+    pq_check_sse(TEST_SOURCES + 2, TEST_LEN * TEST_TIMES, buffs);
+#endif
+#endif
+    endtime = get_time();
   }
-  // do_test_pq_check(buffs);
-  // wait(NULL);
-  while ((tmpPid = wait(&status)) > 0)
-    ;
-  // while (tmpPid = waitpid(-1, NULL, 0))
-  // {
-  //   if (errno == ECHILD)
-  //   {
-  //     break;
-  //   }
-  // }
-  long long endtime = get_time();
+  else // mutiple cores test
+  {
+    starttime = get_time();
+    for (int i = 0; i < PID_NUM; i++)
+    {
+      pid[i] = fork();
+      if (pid[i] > 0)
+      {
+        continue;
+      }
+      else if (pid[i] == 0)
+      {
+#ifdef ALL_IN_CACHE
+        do_test_pq_check(buffs, TEST_TIMES - i * UNIT);
+#else
+#ifdef CHECK_BASELINE
+        pq_check_base(TEST_SOURCES + 2, TEST_LEN * TEST_TIMES, buffs);
+#else
+        pq_check_sse(TEST_SOURCES + 2, TEST_LEN * TEST_TIMES, buffs);
+#endif
+#endif
+        exit(0);
+      }
+      else
+      {
+        printf("fork() failed\n");
+        break;
+      }
+    }
+    // wait(NULL);
+    while ((tmpPid = wait(&status)) > 0)
+      ;
+    // while (tmpPid = waitpid(-1, NULL, 0))
+    // {
+    //   if (errno == ECHILD)
+    //   {
+    //     break;
+    //   }
+    // }
+    endtime = get_time();
+  }
   long long nsec = endtime - starttime;
   long long msec = nsec / 1000000;
-
-  // printf("clock_gettime():%lld\n", nsec);
   printf("校验耗时%lldms\n", msec);
   long long MEM;
+#ifdef ALL_IN_CACHE
   if (TEST_LEN > 1024 * 1024)
     MEM = (TEST_LEN / 1024 / 1024 * TEST_SOURCES) * real_times;
   else
     MEM = (TEST_LEN * TEST_SOURCES / 1024) * real_times / 1024;
+#else
+  // MEM = (TEST_LEN * TEST_SOURCES / 1024) * TEST_TIMES / 1024 * (PID_NUM == 0 ? 1 : PID_NUM);
+  MEM = (TEST_LEN * TEST_TIMES / 1024) * TEST_SOURCES / 1024 * (PID_NUM == 0 ? 1 : PID_NUM);
+#endif
   // MEM = (((TEST_LEN * TEST_SOURCES / 1024) * TEST_TIMES / 1024) + ((TEST_LEN * TEST_SOURCES / 1024 / 1024) * (TEST_TIMES - (PID_NUM - 1) * UNIT))) * PID_NUM / 2;
   printf("MEM%lldMB\n", MEM);
   printf("吞吐率%fMB/s\n", MEM * 1.0 / nsec * 1000000000);
